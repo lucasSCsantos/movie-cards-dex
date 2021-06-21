aa3ba462810d34dc2a540909d68db3a96190a98c
lucasSCsantos <luc.cristovam10@gmail.com>
2021-06-21 14:20:38 -0300

    onHover function

--- src/components/home/Movie.jsx
+++ src/components/home/Movie.jsx
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import '../../styles/Movie.css';
 
 class Movie extends Component {
+	revealMoveInfos({ target }) {
+		const movieInfo = target.firstChild;
+		console.log(movieInfo.style.display)
+		movieInfo.style.display === 'block' ? movieInfo.style.display = '' : movieInfo.style.display = 'block' 
+	}
+
 	render() {
 		const { movie } = this.props;
 		const IMG_BASE = 'https://image.tmdb.org/t/p/w200';
 		return (
 			<div className="movieCard">
-				<div className="movieImg" style={{backgroundImage: `url(${IMG_BASE}${movie.poster_path})`}}>
+				<div className="movieImg" onMouseEnter={({target}) => {
+					target.firstChild.style.display = 'block' 
+				}} 
+				onMouseLeave={({target}) => {
+					target.firstChild.style.display = 'none' 
+				}}
+				style={{backgroundImage: `url(${IMG_BASE}${movie.poster_path})`}}>
 					<div className="movieCardBottom">
 					</div>
 				</div>
