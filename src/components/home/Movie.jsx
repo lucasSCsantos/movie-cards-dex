4f0212fc6e0be6d085250fcc8aaa3377471dfd1a
lucasSCsantos <luc.cristovam10@gmail.com>
2021-06-21 11:58:15 -0300

    page change functions

--- src/components/home/Movie.jsx
+++ src/components/home/Movie.jsx
@@ -4,7 +4,7 @@ import '../../styles/Movie.css';
 class Movie extends Component {
 	render() {
 		const { movie } = this.props;
-		const IMG_BASE = 'https://image.tmdb.org/t/p/w200'
+		const IMG_BASE = 'https://image.tmdb.org/t/p/w200';
 		return (
 			<div className="movieCard">
 				<div className="movieImg" style={{backgroundImage: `url(${IMG_BASE}${movie.poster_path})`}}>
--- src/components/home/MovieList.jsx
+++ src/components/home/MovieList.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import '../../styles/MovieList.css';
 import { getMovies } from '../../services/api';
 import Movie from './Movie';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 class MovieList extends Component {
 	constructor(props) {
@@ -9,13 +11,18 @@ class MovieList extends Component {
 		this.state = {
 			moviesList: [],
 			movieQuant: 9,
+			pages: [],
 		}
 		this.generateMoviesList = this.generateMoviesList.bind(this);
-		this.changePage = this.changePage.bind(this);
+		this.changePageNext = this.changePageNext.bind(this);
+		this.changePagePrevious = this.changePagePrevious.bind(this);
+		this.changePageByNumber = this.changePageByNumber.bind(this);
+		this.createPages = this.createPages.bind(this);
 	}
 
 	componentDidMount() {
 		this.generateMoviesList();
+		this.createPages();
 	}
 	
 	async generateMoviesList() {
@@ -25,21 +32,58 @@ class MovieList extends Component {
 		})
 	}
 
-	changePage() {
+	changePageNext() {
+		const { movieQuant } = this.state;
+		if ( movieQuant < 199) {
+			this.setState((previous) => ({
+				movieQuant: previous.movieQuant + 10,
+			}))
+		}
+	}
+
+	createPages() {
+		const pages = Array.from({ length: 20 }, (v, k) => k + 1)
 		this.setState((previous) => ({
-			movieQuant: previous.movieQuant + 10,
+			pages,
 		}))
 	}
 
+	changePagePrevious() {
+		const { movieQuant } = this.state;
+		if ( movieQuant > 9) {
+			this.setState((previous) => ({
+				movieQuant: previous.movieQuant - 10,
+			}))
+		}
+	}
+
+	changePageByNumber({ target }) {
+		const { innerText } = target;
+		const pageNumber = Number.parseInt(innerText);
+		this.setState({
+			movieQuant: (9 + ((pageNumber - 1) * 10)),
+		})
+	}
+
 	render() {
-		const { moviesList, movieQuant } = this.state;
+		const { moviesList, movieQuant, pages } = this.state;
 		console.log(moviesList)
 		return (
-			<div className="movieList">
-				{moviesList.slice((movieQuant-9), movieQuant).map((movie, index) => (
-					<Movie movie={movie} key={index} />
-				))}
-				<button onClick={this.changePage}></button>
+			<div className="movieListArea">
+				<div className="movieList">
+					{moviesList.slice((movieQuant-9), movieQuant).map((movie, index) => (
+						<Movie movie={movie} key={index} />
+					))}
+				</div>
+					<button type="button" onClick={this.changePagePrevious}>
+						<FontAwesomeIcon icon={faChevronLeft} />
+					</button>
+					{pages.map((page, index) => (
+						<button key={index} type="button" onClick={this.changePageByNumber}>{page}</button>
+					))}
+					<button type="button" onClick={this.changePageNext}>
+					<FontAwesomeIcon icon={faChevronRight} />
+					</button>
 			</div>
 		);
 	}
